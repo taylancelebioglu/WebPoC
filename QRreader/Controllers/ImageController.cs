9c44d221bc416fb5f37f6ac6636af8edddeb103b
taylancelebioglu <taylancelebioglu@gmail.com>
2017-11-02 09:59:55 +0300

    Done

--- QRreader/Controllers/ImageController.cs
+++ QRreader/Controllers/ImageController.cs
@@ -1,17 +1,59 @@
-﻿using System;
+﻿using QRreader.Models;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ZXing;
 
 namespace QRreader.Controllers
 {
     public class ImageController : Controller
     {
-        // GET: Image
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Display(ImageFile img)
+        {
+            return View(img);
+        }
+        [HttpPost]
+        public ActionResult Create(ImageFile img, HttpPostedFileBase file)
+        {
+            if (ModelState.IsValid)
+            {
+                if (file != null)
+                {
+                    string path = HttpContext.Server.MapPath("~/Images/") + file.FileName;
+                    file.SaveAs(path);
+                    img.ImagePath = path;
+                }
+
+                IBarcodeReader reader = new BarcodeReader();
+                // load a bitmap
+                var barcodeBitmap = (Bitmap)Bitmap.FromFile(img.ImagePath);
+                // detect and decode the barcode inside the bitmap
+                var result = reader.Decode(barcodeBitmap);
+                // do something with the result
+                if (result != null)
+                {
+                    img.BarcodeFormat = result.BarcodeFormat.ToString();
+                    img.BarcodeText = result.Text;
+                }
+                return View("Display", img);
+
+                //return RedirectToAction("Display", new { img = img });
+            }
+            return View("Display", img);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View("Upload");
         }
     }
 }
---
+++ QRreader/Models/ImageFile.cs
@@ -0,0 +1,10 @@
+﻿namespace QRreader.Models
+{
+    public partial class ImageFile
+    {
+        public int ID { get; set; }
+        public string ImagePath { get; set; }
+        public string BarcodeFormat { get; set; }
+        public string BarcodeText { get; set; }
+    }
+}
